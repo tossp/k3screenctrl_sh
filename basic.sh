6007655af3133db015a083f9bce555f970d7ef80
小山 <lufanzhong@gmail.com>
2017-05-29 23:56:36 +0800

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+. /etc/os-release
+
+PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
+PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
+
+WAN_IFNAME=$(uci get network.wan.ifname)
+MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
+
+CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
+
+HW_VERSION=${LEDE_DEVICE_REVISION:0:2}
+FW_VERSION=${BUILD_ID:0:17}
+
+echo $PRODUCT_NAME
+
+disp=$(uci get k3screenctrl.@general[0].disp_cputemp) 
+
+if [ $disp -eq 1 ]; then
+echo $HW_VERSION $CPU_TEMP
+else
+echo $HW_VERSION
+fi
+
+echo $FW_VERSION
+echo $MAC_ADDR
