280c4f3fb3cf863151e53488006089961e141af4
小山 <lufanzhong@gmail.com>
2017-05-29 23:57:40 +0800

    Add files via upload

---
+++ update_oui.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+wget https://cdn.mivm.cn/OpenWrt/k3screenctrl/oui.txt.md5 -O /tmp/oui.txt.md5
+if [ -s /tmp/oui.txt.md5 ]; then
+	md5=$(cat /tmp/oui.txt.md5 | awk '{print $2}' | tr '[A-Z' '[a-z]')
+	md5sum=$(md5sum /lib/k3screenctrl/oui/oui.txt | awk '{print $1}')
+	if [ "$md5" != "$md5sum" ]; then
+		wget https://cdn.mivm.cn/OpenWrt/k3screenctrl/oui.txt -O /tmp/oui.txt
+		if [ -s /tmp/oui.txt ]; then
+			mv /tmp/oui.txt /lib/k3screenctrl/oui/oui.txt
+			logger 屏幕设备图标 MAC 识别库 oui.txt 更新完成
+		fi
+	fi
+else
+	logger 屏幕设备图标 MAC 识别库 oui.txt 校验文件下载失败 无法更新
+fi
